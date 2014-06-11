4431359417896c3998dd3fdbca8e059a4ca66913
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-06-11 15:18:42 -0700

    Added an eachPermutation function that calls an iterator on each possible permutation of a passed-in list

---
+++ eachPermutation.js
@@ -0,0 +1,28 @@
+//Calls an iterator on each possible permutation
+//of the passed-in values array
+var eachPermute = function(values, iterator) {
+
+  var innerPermute = function(permutation, remainingValues) {
+    if (remainingValues.length === 0) {
+      iterator(permutation);
+    } else {
+      for (var i=0; i<remainingValues.length; i++) {
+        //Create copy of remainingValues
+        var newRemaining = remainingValues.slice();
+
+        //Remove and save the ith entry in the new remaining values array
+        var toPush = newRemaining.splice(i,1);
+
+        //Creates a copy of the permutation list so far
+        var newPermutation = permutation.slice();
+
+        //Adds the new value to the new permutation list
+        newPermutation.push(toPush);
+
+        //Recursively calls this function with the new lists
+        innerPermute(newPermutation, newRemaining);
+      }
+    }
+  };
+  innerPermute([], values);
+};
